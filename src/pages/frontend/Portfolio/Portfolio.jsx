00f07d474337cf76d3c3bc9fdb01e74af325037f
Mradul Sharma <15956546+ermradulsharma@users.noreply.github.com>
2025-08-29 20:58:02 +0530

    portfolio

--- src/pages/frontend/Portfolio/Portfolio.jsx
+++ src/pages/frontend/Portfolio/Portfolio.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Container, Row, Col, Card, Button, Badge } from "react-bootstrap";
+import { Container, Row, Col, Card, Button, Badge, Modal } from "react-bootstrap";
 import "../../frontend/Portfolio/Portfolio.css";
 import data from "../../../components/database/portfolio.json";
 
@@ -21,11 +21,22 @@ const truncateText = (text, maxLength = 155) => {
 export default function Portfolio() {
     const { title, description, period, projectsList } = data.projects;
     const [filter, setFilter] = useState("All");
+    const [showModal, setShowModal] = useState(false);
+    const [activeProject, setActiveProject] = useState(null);
     const filteredProjects = projectsList.filter((p) =>
         filter === "All" ? true : p.technologies.some((tech) =>
             tech.toLowerCase().includes(filter.toLowerCase())
         )
     );
+    const handleShow = (project) => {
+        setActiveProject(project);
+        setShowModal(true);
+    };
+
+    const handleClose = () => {
+        setActiveProject(null);
+        setShowModal(false);
+    };
     return (
         <section className="portfolio-section py-0">
             {/* Hero Section */}
@@ -46,10 +57,14 @@ export default function Portfolio() {
                 </header>
                 <Row xs={1} md={2} lg={4} className="g-4 portfolio-project">
                     {filteredProjects.map((p, index) => (
-                        <Col key={index}>
-                            <Card className="h-100 shadow-sm border-0 project-card p-0">
-                                <div className="ratio ratio-21x9 overflow-hidden rounded-top">
-                                    <Card.Img src={require(`../../../assets/projects/${p.image}`)} alt={p.title} className="object-fit-cover rounded-0" />
+                        <Col key={index} className="rounded">
+                            <Card className="h-100 shadow-lg border-0 project-card p-0 rounded">
+                                <div className="overflow-hidden rounded-top">
+                                    <Card.Img src={require(`../../../assets/projects/${p.image}`)} alt={p.title} className="object-fit-cover rounded-0" height={147} />
+                                    {/* Hover Overlay with Button */}
+                                    <div className="overlay d-flex justify-content-center align-items-center">
+                                        <Button variant="success" onClick={() => handleShow(p)} size="sm" bg="success-subtle">Case Study</Button>
+                                    </div>
                                 </div>
                                 <Card.Body>
                                     <div className="d-flex justify-content-between align-items-start">
@@ -59,18 +74,109 @@ export default function Portfolio() {
                                     <Card.Text className="text-muted mb-2">{truncateText(p.description, 155)}</Card.Text>
                                     <TechBadges items={p.technologies} />
                                 </Card.Body>
-                                <Card.Footer className="border-0">
+                                {/* <Card.Footer className="border-0">
                                     <div className="d-flex align-items-center justify-content-between">
                                         <Button as="a" href={p.link} target="_blank" rel="noreferrer" variant="primary" size="sm">Live Demo</Button>
                                         <Button as="a" href={p.link} target="_blank" rel="noreferrer" variant="danger" size="sm">Case Study</Button>
                                         <Button as="a" href={p.link} target="_blank" rel="noreferrer" variant="success" size="sm">Gallary</Button>
                                     </div>
-                                </Card.Footer>
+                                </Card.Footer> */}
                             </Card>
                         </Col>
                     ))}
                 </Row>
             </Container>
+            {/* Modal */}
+            <Modal show={showModal} onHide={handleClose} centered size="lg">
+                <Modal.Header closeButton className="p-3">
+                    <Modal.Title className="fs-6 fw-semibold">{activeProject?.title}</Modal.Title>
+                </Modal.Header>
+                <Modal.Body className="px-3">
+                    {/* Overview */}
+                    {activeProject?.case_study?.overview && (
+                        <>
+                            <h6 className="fw-bold">Overview</h6>
+                            <p className="text-muted">{activeProject.case_study.overview}</p>
+                        </>
+                    )}
+
+                    {/* Problem Statement */}
+                    {activeProject?.case_study?.problem_statement?.length > 0 && (
+                        <>
+                            <h6 className="fw-bold mt-3">Problem Statement</h6>
+                            <ul>
+                                {activeProject.case_study.problem_statement.map((problem, idx) => (
+                                    <li key={idx}>{problem}</li>
+                                ))}
+                            </ul>
+                        </>
+                    )}
+
+                    {/* Objectives */}
+                    {activeProject?.case_study?.objectives?.length > 0 && (
+                        <>
+                            <h6 className="fw-bold mt-3">Objectives</h6>
+                            <ul>
+                                {activeProject.case_study.objectives.map((obj, idx) => (
+                                    <li key={idx}>{obj}</li>
+                                ))}
+                            </ul>
+                        </>
+                    )}
+
+                    {/* Tech Stack & Key Features */}
+                    {activeProject?.case_study?.implemented && (
+                        <>
+                            <h6 className="fw-bold mt-3">{activeProject.case_study.implemented.title}</h6>
+                            <ul>{activeProject.case_study.implemented.key_features.map((f, idx) => (<li key={idx}>{f}</li>))}</ul>
+                        </>
+                    )}
+                    {/* Challenges & Solutions */}
+                    {activeProject?.case_study?.challenges_and_solutions?.length > 0 && (
+                    <>
+                        <h6 className="fw-bold mt-3">Challenges & Solutions</h6>
+                        <div className="table-responsive">
+                        <table className="table table-bordered table-sm align-middle">
+                            <thead className="table-light">
+                            <tr>
+                                <th className="text-center" style={{ width: "50%"}}>Challenge</th>
+                                <th className="text-center" style={{ width: "50%"}}>Solution</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            {activeProject.case_study.challenges_and_solutions.map((item, idx) => (
+                                <tr key={idx}>
+                                    <td>{item.challenge}</td>
+                                    <td>{item.solution}</td>
+                                </tr>
+                            ))}
+                            </tbody>
+                        </table>
+                        </div>
+                    </>
+                    )}
+
+
+                    {/* Outcomes */}
+                    {activeProject?.case_study?.outcomes?.length > 0 && (
+                        <>
+                            <h6 className="fw-bold mt-3">Outcomes</h6>
+                            <ul>{activeProject.case_study.outcomes.map((outcome, idx) => (<li key={idx}>{outcome}</li>))}</ul>
+                        </>
+                    )}
+
+                    {/* Tech Badges */}
+                    {activeProject?.technologies?.length > 0 && (
+                        <div className="mt-3 d-flex flex-wrap gap-2">
+                            {activeProject.technologies.map((tech, i) => (<Badge bg="success-subtle" text="dark" className="fw-normal" key={i}>{tech} </Badge>))}
+                        </div>
+                    )}
+                </Modal.Body>
+                <Modal.Footer className="p-2 border-0">
+                    <Button variant="danger" size="sm" className="m-0 px-4" onClick={handleClose}>Close</Button>
+                </Modal.Footer>
+            </Modal>
+
         </section>
     );
 }
